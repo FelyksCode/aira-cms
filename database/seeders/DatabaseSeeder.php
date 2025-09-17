7d18bdf6241672744e1fb78f0d7457540833ab4b
Felyks <gamingfelix78@gmail.com>
2025-09-17 13:51:56 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => env(USER_NAME, 'demo'),
-            'email' => env(USER_EMAIl, 'demo@demo.com'),
-            'password' => Hash::make(env(USER_PASSWORD, "123")),
+            'name' => env('USER_NAME', 'demo'),
+            'email' => env('USER_EMAIl', 'demo@demo.com'),
+            'password' => Hash::make(env('USER_PASSWORD', "123")),
         ]);
     }
 }
