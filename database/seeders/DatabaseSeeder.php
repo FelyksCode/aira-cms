b52ae226c4e6e970a35b78498987df6aa1e192b4
Felyks <gamingfelix78@gmail.com>
2025-09-17 13:35:36 +0700

    Update Readme and Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +17,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => env(USER_NAME, 'demo'),
+            'email' => env(USER_EMAIl, 'demo@demo.com'),
+            'password' => Hash::make(env(USER_PASSWORD, "123")),
         ]);
     }
 }
