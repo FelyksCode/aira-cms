d15fe863c50f99844e1199322010c19390dd355e
Felyks <gamingfelix78@gmail.com>
2025-09-18 19:26:15 +0700

    Update AdminPanelProvider.php

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,7 +25,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('cms/admin')
             ->login()
             ->colors([
                 'primary' => Color::Sky,
