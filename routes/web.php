fd6d90ace509e2d5c1cec446d68583320a2e0a0f
Felyks <gamingfelix78@gmail.com>
2025-09-24 11:32:50 +0700

    Remove FHIR Controller

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,3 @@
 <?php
 
-use App\Http\Controllers\FhirController;
 use Illuminate\Support\Facades\Route;
-
-Route::middleware('auth')->group(function () {
-    Route::get('/kyc_url', [FhirController::class, 'kyc_url'])->name('kyc_url');
-});
